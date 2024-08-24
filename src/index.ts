419cf2d72cca91acfd77399deb86dc28d1c0fca5
Shourya Pal <palshourya897@gmail.com>
2024-08-24 22:42:00 +0530

    fix: env config

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URl as string  );
+    await mongoose.connect(process.env.MONGO_URL as string  );
     console.log("Database is connected successfully!");
   } catch (err) {
     console.error("Database connection error:", err);
