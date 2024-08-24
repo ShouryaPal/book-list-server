7cc186722c43598c0dcb3ce664035cc8fa3ce085
Shourya Pal <palshourya897@gmail.com>
2024-08-24 22:55:34 +0530

    fix: fix some cors error

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://book-list-client-psi.vercel.app/",
+    origin: "https://book-list-client-psi.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
